09d0f844337fe538c5064c23bbc4caad2d40e061
Student_MA118 <Student_MA118@Student-MA118.local>
2020-04-16 19:25:55 -0400

    Working on toggle portion

---
+++ src/components/HamburgerMenu/Toggle.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Toggle.css';
+
+
+const ToggleButton = props => (
+    <button className = 'toggle-butt'>
+        <div className = 'toggle-butt-row'> </div>
+        <div className = 'toggle-butt-row'> </div>
+        <div className = 'toggle-butt-row'> </div>
+    </button>
+)
+
+export default ToggleButton;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './Navbar.css';
+import '../HamburgerMenu/Toggle.js'
+import ToggleButton from '../HamburgerMenu/Toggle.js';
 
 function Navbar (props) {
     return (
         <header className = 'toolbar'>
         <nav className = 'navigator'> 
+            <div>
+                <ToggleButton/>
+            </div>
             <div className = 'home-link'> <a href="./"> Navbar to be </a> </div>
             <div className = 'wrapper'> </div>
             <div className = 'multi-link'>
