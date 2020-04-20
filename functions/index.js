3815ca1435cfd6748afd497165f3b2261771aaf6
Student_MA118 <Student_MA118@Student-MA118.local>
2020-04-20 17:36:27 -0400

    Added additional functions for Firebase

--- functions/index.js
+++ functions/index.js
@@ -8,7 +8,11 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
 });
 
 exports.getPosts = functions.https.onRequest((request, response) => {
-    admin.firestore().collection('posts').get().then((data) => {
+    admin
+    .firestore()
+    .collection('posts')
+    .get()
+    .then((data) => {
         let posts = [];
         data.forEach((document) => {
             posts.push(document.data());
@@ -17,3 +21,24 @@ exports.getPosts = functions.https.onRequest((request, response) => {
     })
     .catch((error) => console.error(error));
 }); 
+
+exports.createPosts = functions.https.onRequest((request, response) => {
+    const newPosts = {
+        body : request.body.body,
+        userName : request.body.userName,
+        createdOn : admin.firestore.Timestamp.fromDate(new Date())
+        // location : request.body.location
+    };
+    admin
+    .firestore()
+    .collection('posts')
+    .add(newPosts)
+    .then((document) => {
+        response.json({message : `Great job, document ${document.id} created successfully`})
+    })
+    .catch((error) => {
+        response.status(500).json({error: "Oops, something went wrong!"});
+        console.error(error);
+        });
+    });
+    
